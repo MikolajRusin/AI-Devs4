644ab1b1020f389226d7d7882df73fd66f9b2d46
MikolajRusin <rusinmiko@gmail.com>
2026-03-16 23:31:46 +0100

    created ai devs hub client

---
+++ 01-Programming_the_operation_of_laguage_model/s01e04_s01e04_support_for_multimodality_and_attachments/src/clients/hub_client.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import httpx
+
+
+class AiDevsHubClient:
+    def __init__(self, api_key: str, base_url: str, data_dir: str | Path):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.data_dir_path = Path(data_dir)
+        self.downloads_dir_path = self.data_dir_path / 'downloaded_data'
+        self.downloads_dir_path.mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    async def _get(url: str) -> httpx.Response:
+        async with httpx.AsyncClient(timeout=20) as client:
+            response = await client.get(url)
+            response.raise_for_status()
+        return response
+
+    async def download_data(self, doc_name: str) -> str:
+        url = f'{self.base_url}/dane/doc/{doc_name}'
+        response = await self._get(url)
+
+        dest_path = self.downloads_dir_path / doc_name
+        if dest_path.suffix == '.md':
+            dest_path.write_text(response.text, encoding='utf-8')
+        else:
+            dest_path.write_bytes(response.content)
+
+        return str(dest_path)
